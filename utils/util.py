0f77a67b67adf3847fe843737bfe800bd74ae870
Rohan R <rohanrathan21@gmail.com>
2022-06-04 22:34:51 +0530

    Utility function to create and save AES key

--- utils/util.py
+++ utils/util.py
@@ -1,4 +1,5 @@
 import yaml
+from Crypto.Random import get_random_bytes
 
 
 def get_credentials():
@@ -7,3 +8,21 @@ def get_credentials():
             return yaml.safe_load(f)
     except yaml.YAMLError as ex:
         print(f"Error occured while loading credentials config: {ex}")
+
+
+# ! Any alternate way to do this in a single pass.
+# ! and without saving the key
+
+
+def generate_aes_key():
+    with open("security/aes_key", "wb") as f:
+        key = get_random_bytes(16)
+        f.write(key)
+    return key
+
+
+def generate_iv():
+    with open("security/iv", "wb") as f:
+        iv = get_random_bytes(16)
+        f.write(iv)
+    return iv
