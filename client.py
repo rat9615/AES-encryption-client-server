eb67c2ff7b1abca7a0d76400aa47b03340c92160
Rohan R <rohanrathan21@gmail.com>
2022-06-04 22:19:02 +0530

    Initiate handshake from client to server socket

--- client.py
+++ client.py
@@ -1,4 +1,8 @@
 import socket
+from utils import util
+from time import sleep
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP as oaep
 
 
 def create_connection():
@@ -9,5 +13,23 @@ def create_connection():
     return server
 
 
+def encrypt_password():
+    with open("security/public_key.pem", "rb") as file_pub:
+        key = RSA.import_key(file_pub.read())
+
+    credentials = util.get_credentials()
+    message = str.encode(credentials['app']['password'])
+    encrypted_password = oaep.new(key).encrypt(message)
+    return encrypted_password
+
+
+def initialize_handshake():
+    message = encrypt_password()
+    server.send(message)
+    print("\nInitiated handshake")
+
+
 if __name__ == "__main__":
     server = create_connection()
+    initialize_handshake()
+    sleep(3)
--- server.py
+++ server.py
@@ -1,5 +1,8 @@
 import socket
+from utils import util
 from security.keys import generate_keys
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP as oaep
 
 
 def bind_connection():
@@ -11,6 +14,25 @@ def bind_connection():
     return server
 
 
+def decrypt_password(message):
+    with open("security/private_key.pem", "rb") as file_priv:
+        key = RSA.import_key(file_priv.read())
+    decrypted_password = oaep.new(key).decrypt(message)
+    return decrypted_password.decode("utf-8")
+
+
+def is_handshake_complete():
+    message = client.recv(1024)
+    dcrypt_message = decrypt_password(message)
+    print(f"Secret password is: {dcrypt_message}")
+    credentials = util.get_credentials()
+    if dcrypt_message != credentials['app']['password']:
+        print("Handshake failed. Disconnecting from client")
+        return False
+    print("Handshake successful!")
+    return True
+
+
 if __name__ == "__main__":
     generate_keys()
     server = bind_connection()
@@ -18,3 +40,8 @@ if __name__ == "__main__":
     while True:
         client, address = server.accept()
         print(f"\nEstablished connection request from client: {address}")
+
+        if not is_handshake_complete():
+            server.shutdown(socket.SHUT_RDWR)
+            server.close()
+            exit(0)
